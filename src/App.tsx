d5b5f7e3d016e2695f49fdf6ea69d567efd8302e
JonFerri <jftonymm@gmail.com>
2022-08-11 18:48:32 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Route } from "wouter";
+import Home from "./Pages/Home/Home";
+import ResultsGiffs from "./Pages/ResultsGiffs/ResultsGiffs";
+import SingleGiff from "./Pages/SingleGiff/SingleGiff";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Route path='/' component={Home} />
+      <Route path='/giffs/:keyword' component={ResultsGiffs} />
+      <Route path="/giffs/singleGiff/:id" component={SingleGiff} />
     </div>
   );
 }
 
 export default App;
+
