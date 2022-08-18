0b3a42fbd668864989e846d2b5645bcd37d1211a
JonFerri <jftonymm@gmail.com>
2022-08-18 13:23:49 +0200

    Added React.lazy and React.suspense to the trending searches section

--- src/Components/TrendingSearches/LazyTrending.tsx
+++ src/Components/TrendingSearches/LazyTrending.tsx
@@ -1,18 +1,25 @@
-import "./TrendingSearches.css";
-import TrendingSearches from "./TrendingSearches";
-import useLazy from "Hooks/useLazy";
+import "./TrendingSearches.css"
+import React from "react"
+//import TrendingSearches from "./TrendingSearches"
+import useLazy from "Hooks/useLazy"
+//import Loading from "Components/Loading/Loading"
+const TrendingSearches = React.lazy(()=> {
+  return import('./TrendingSearches')
+})
 
 const LazyTrending = () => {
-  const { show, fromRef } = useLazy({});
+  const { show, fromRef } = useLazy({})
 
-  return (
-   
-    <div ref={fromRef}>
-        {
-            show ? <TrendingSearches /> : null
-        }
-    </div>
+  return(
+      
+        <React.Suspense fallback={<div ref={fromRef}></div>}>
+          <div ref={fromRef}>
+            {show ? <TrendingSearches /> : null}
+          </div>
+        </React.Suspense>
+      
+    
   ) 
-};
+}
 
-export default LazyTrending;
+export default LazyTrending
