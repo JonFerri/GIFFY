9598d06a5d3d1792c99896cb8b8acce07250b7ce
JonFerri <jftonymm@gmail.com>
2022-08-23 19:38:15 +0200

    first use of useReducer.

--- src/Hooks/useGiffs.tsx
+++ src/Hooks/useGiffs.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext } from "react";
+import { useState, useEffect, useContext,useReducer } from "react";
 import GetGiffs from "../Services/GetGiffs";
 import { GiffContext } from "../Context/GiffContext";
 
@@ -6,36 +6,68 @@ import { GiffContext } from "../Context/GiffContext";
 function useGiffs(keyword:string) {
   
 
+  interface  StateReducer {
+    isRandom: boolean,
+    isLoading: boolean,
+    page: number
+  }
+  const INITIAL_VALUES: StateReducer = {
+    isRandom: false,
+    isLoading: false,
+    page: 1
+  }
+  const ACTION_TYPES = {
+     SET_IS_RANDOM: "setIsRandom",
+     SET_IS_LOADING: "setIsLoading"
+  }
 
+  function reducer (currentState:StateReducer, action:any){
+      switch (action.type) {
+        case ACTION_TYPES.SET_IS_RANDOM:
+          return {
+            ...currentState,
+            isRandom: action.payload.value
+          }
+
+        case ACTION_TYPES.SET_IS_LOADING:
+          return {
+            ...currentState,
+            isLoading: action.payload.value
+          }
+      
+      }
+  }
   const { giffs, setGiffs } = useContext(GiffContext);
-  const [isRandom, setIsRandom ] = useState<boolean>(false);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [page, setPage] = useState<number>(1)
-
+  
+  //useReducer
+  const [state, dispatch] = useReducer<React.Reducer<any,any>>(reducer,INITIAL_VALUES)
+  
   useEffect(() => {
-    
-    setIsLoading(true);
+    dispatch({type: ACTION_TYPES.SET_IS_LOADING, payload: {value:true}})
     GetGiffs({keyword, limit:10})
       .then(res => {
         setGiffs(res);
-        setIsLoading(false);
-        if (keyword === "random") setIsRandom(true)
+        dispatch({type: ACTION_TYPES.SET_IS_LOADING, payload: {value:false}})
+        if (keyword === "random") dispatch({type:ACTION_TYPES.SET_IS_RANDOM,payload: {value: true}})
         localStorage.setItem("lastKeyword", keyword)
     });
-  }, [keyword, setGiffs]);
+  }, [ACTION_TYPES.SET_IS_LOADING, ACTION_TYPES.SET_IS_RANDOM, keyword, setGiffs]);
 
   useEffect(()=>{
    
     if (page === 1) return
     
-    setIsLoading(true);
+    dispatch({type: ACTION_TYPES.SET_IS_LOADING, payload: {value:true}})
     GetGiffs({keyword, limit: 10, page}).then(res => {
       setGiffs(prevGiffs => prevGiffs.concat(res));
-      setIsLoading(false);
+      dispatch({type: ACTION_TYPES.SET_IS_LOADING, payload: {value:false}})
       
     });
-  }, [keyword, page, setGiffs])
-
+  }, [ACTION_TYPES.SET_IS_LOADING, keyword, page, setGiffs])
+  
+  const { isRandom, isLoading } = state;
+  
   return { giffs, isLoading, isRandom, keyword, page, setPage };
 }
 
